85fe20f2588f270db38ada6397e1f0b83699edfc
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-17 17:26:24 +0200

    add docs to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DocsComponent } from './docs/docs.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'docs', component: DocsComponent}
 ];
