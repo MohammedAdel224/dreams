18266d35a94cbe87dcb3462c655f23fd3ada957c
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-18 10:06:24 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocsComponent } from './docs/docs.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { SlidersComponent } from './sliders/sliders.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'docs', component: DocsComponent}
+    { path: 'docs',
+        component: DocsComponent,
+        children:[
+            { path: 'get-started', component: GetStartedComponent },
+            { path: 'components/sliders', component: SlidersComponent },
+            { path: '', redirectTo: 'get-started', pathMatch: 'full'}
+        ]
+    }
 ];
