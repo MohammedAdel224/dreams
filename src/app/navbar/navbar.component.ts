48d5249e95e18f2185b9fb0e97a7dd61e2bc4d46
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-19 15:43:00 +0200

    get current path

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [RouterModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    imports: [RouterModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+    currentPath: string = '';
 
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.currentPath = event.urlAfterRedirects; // Update on route change
+            }
+        });
+    }
 }
