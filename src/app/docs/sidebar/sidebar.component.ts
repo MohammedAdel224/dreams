a14090e36f19075a2317c9b1fb6d13e279a90efa
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-19 22:00:02 +0200

    add brand name to offcanvas header

--- src/app/docs/sidebar/sidebar.component.ts
+++ src/app/docs/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { BrandNameComponent } from '../../brand-name/brand-name.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule],
+  imports: [RouterModule, BrandNameComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
