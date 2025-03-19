80d168153bf1aaa93e0429c4b0b71b26a9575fee
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-19 22:01:49 +0200

    fix indent

--- src/app/docs/sidebar/sidebar.component.ts
+++ src/app/docs/sidebar/sidebar.component.ts
@@ -3,10 +3,10 @@ import { Router, RouterModule } from '@angular/router';
 import { BrandNameComponent } from '../../brand-name/brand-name.component';
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [RouterModule, BrandNameComponent],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+    selector: 'app-sidebar',
+    imports: [RouterModule, BrandNameComponent],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
