000ad179bb8afe5c44942d2f760fe2777c7b644e
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-22 13:39:02 +0200

    turn get started component into introduction

---
+++ src/app/docs/get-started/introduction/introduction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BrandNameComponent } from '../../../brand-name/brand-name.component';
+
+@Component({
+  selector: 'app-introduction',
+  imports: [BrandNameComponent],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
