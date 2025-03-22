7445a1e0b0326547437a544de1a92302735ce2c4
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-22 15:22:32 +0200

    implement assertBetween0And100

--- src/dreams/src/helpers/util.ts
+++ src/dreams/src/helpers/util.ts
@@ -8,3 +8,12 @@ export function toPixel(value: string, context = document.documentElement) {
     context.removeChild(div);
     return `${pixels}px`;
 }
+
+export function assertBetween0And100(number: number){
+    if (typeof number !== "number" || isNaN(number)) {
+        throw new Error("Invalid input: Value must be a number.");
+    }
+    if (number < 0 || 100 < number) {
+        throw new Error("Invalid input: Number must be between 0 and 100.");
+    }
+}
