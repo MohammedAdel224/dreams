bbbe9505fa3b777c30a5ac6443a5fa227acc8909
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-22 17:04:10 +0200

    implement OneSideSlider

---
+++ src/dreams/src/sliders/src/ts/oneSideSlider.ts
@@ -0,0 +1,33 @@
+import OneSideTrack from "./oneSideTrack";
+import Slider from "./slider";
+
+class OneSideSlider{
+    #slider: Slider;
+    #track: OneSideTrack;
+
+    constructor(element: HTMLDivElement){
+        const track = element.querySelector(".track") as HTMLDivElement;
+        const slider = element.querySelector("input.slider[type=range]") as HTMLInputElement;
+
+        this.#track = new OneSideTrack(track);
+        this.#slider = new Slider(slider);
+
+        this.#init(track, slider);
+    }
+
+    #init(track: HTMLDivElement, slider: HTMLInputElement){
+        this.#slider.addEventListener("input", ()=>{this.#fillTrack();});
+        this.#fillTrack();
+    }
+
+    #fillTrack(){
+        const trackLength = this.#track.width;
+        const thumbTrackLength = this.#track.width - this.#slider.thumbWidth;
+        const percent = (this.#slider.value - this.#slider.min) / (this.#slider.max - this.#slider.min);
+        const offset = (trackLength - thumbTrackLength) / (2 * trackLength);
+        const adjustedPercent = thumbTrackLength / trackLength * percent + offset;
+        this.#track.fill(adjustedPercent * 100);
+    }
+}
+
+export default OneSideSlider;
