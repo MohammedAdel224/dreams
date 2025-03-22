50756fd48e6cab59e4758b7d25b3e3e910a439c1
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-22 13:42:08 +0200

    add route to download

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocsComponent } from './docs/docs.component';
-import { SlidersComponent } from './sliders/sliders.component';
 import { GetStartedComponent } from './docs/get-started/get-started.component';
+import { SlidersComponent } from './docs/components/sliders/sliders.component';
+import { DownloadComponent } from './docs/get-started/download/download.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
         children:[
             { path: 'get-started', component: GetStartedComponent },
             { path: 'components/sliders', component: SlidersComponent },
+            { path: 'get-started/download', component: DownloadComponent},
             { path: '', redirectTo: 'get-started', pathMatch: 'full'}
         ]
     },
