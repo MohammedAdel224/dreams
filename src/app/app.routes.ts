79f2c9d2269365dd96d68a34fd21df4492bf446a
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-22 13:47:24 +0200

    update route to introduction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocsComponent } from './docs/docs.component';
-import { GetStartedComponent } from './docs/get-started/get-started.component';
 import { SlidersComponent } from './docs/components/sliders/sliders.component';
 import { DownloadComponent } from './docs/get-started/download/download.component';
+import { IntroductionComponent } from './docs/get-started/introduction/introduction.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'docs',
         component: DocsComponent,
         children:[
-            { path: 'get-started', component: GetStartedComponent },
+            { path: 'get-started/introduction', component: IntroductionComponent },
             { path: 'components/sliders', component: SlidersComponent },
             { path: 'get-started/download', component: DownloadComponent},
-            { path: '', redirectTo: 'get-started', pathMatch: 'full'}
+            { path: '', redirectTo: 'get-started/introduction', pathMatch: 'full'}
         ]
     },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
