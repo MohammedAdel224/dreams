177aa45a66375290db57e6f08911b4ddcbaeff0b
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-23 17:55:33 +0200

    devide slider component into parts

---
+++ src/app/docs/components/sliders/customize-colors/customize-colors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customize-colors',
+  imports: [],
+  templateUrl: './customize-colors.component.html',
+  styleUrl: './customize-colors.component.css'
+})
+export class CustomizeColorsComponent {
+
+}
