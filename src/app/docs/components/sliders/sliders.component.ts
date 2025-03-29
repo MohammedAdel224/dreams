3c1b10909b61b7d09a4a59db619ae1d88ee44707
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-29 12:08:01 +0200

    import default style component in sliders

--- src/app/docs/components/sliders/sliders.component.ts
+++ src/app/docs/components/sliders/sliders.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { CreateSliderComponent } from './create-slider/create-slider.component';
 import { CustomizeColorsComponent } from './customize-colors/customize-colors.component';
 import { CustomizeShapeComponent } from './customize-shape/customize-shape.component';
+import { DefaultStyleComponent } from './default-style/default-style.component';
 
 @Component({
   selector: 'app-sliders',
-  imports: [ CreateSliderComponent, CustomizeColorsComponent, CustomizeShapeComponent],
+  imports: [
+    CreateSliderComponent,
+    CustomizeColorsComponent,
+    CustomizeShapeComponent,
+    DefaultStyleComponent],
   templateUrl: './sliders.component.html',
   styleUrl: './sliders.component.css'
 })
