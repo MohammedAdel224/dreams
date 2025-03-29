14a830e130c6046ffa3b170e55bcfde5cf5a58cb
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-29 11:54:12 +0200

    init sliders default style

---
+++ src/app/docs/components/sliders/default-style/default-style.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-style',
+  imports: [],
+  templateUrl: './default-style.component.html',
+  styleUrl: './default-style.component.css'
+})
+export class DefaultStyleComponent {
+
+}
