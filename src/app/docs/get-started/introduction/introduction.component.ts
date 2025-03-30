53c66b32dbbb10a7f1bd8859256b333378d6a8cf
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-30 23:32:39 +0200

    read init-index-html.html asset

--- src/app/docs/get-started/introduction/introduction.component.ts
+++ src/app/docs/get-started/introduction/introduction.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrandNameComponent } from '../../../brand-name/brand-name.component';
+import { UtilService } from '../../../util.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-introduction',
-  imports: [BrandNameComponent],
-  templateUrl: './introduction.component.html',
-  styleUrl: './introduction.component.css'
+    selector: 'app-introduction',
+    imports: [BrandNameComponent],
+    templateUrl: './introduction.component.html',
+    styleUrl: './introduction.component.css'
 })
-export class IntroductionComponent {
+export class IntroductionComponent implements OnInit{
+    initIndexHTML: string = '';
 
+    constructor(
+        private util: UtilService,
+    ){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/init-index-html.html').subscribe({
+            next: (response)=>{
+                this.initIndexHTML = response;
+                console.log(this.initIndexHTML);
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
