8386412471d31c301af4fca1c627b4ae9ec00f01
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-30 23:29:47 +0200

    implement read file

--- src/app/util.service.ts
+++ src/app/util.service.ts
@@ -1,5 +1,7 @@
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -8,9 +10,14 @@ export class UtilService {
 
     constructor(
         private clipboard: Clipboard,
+        private http: HttpClient,
     ) { }
 
     copyText(text: string){
         this.clipboard.copy(text);
+    }
+
+    readFile(path: string): Observable<string>{
+        return this.http.get(path, {responseType: 'text'});
     }
 }
