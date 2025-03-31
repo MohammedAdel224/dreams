be81b388b76f5103ebe82b8bd8fae45b67f09fad
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 19:54:32 +0200

    use code component

--- src/app/docs/components/sliders/default-style/default-style.component.ts
+++ src/app/docs/components/sliders/default-style/default-style.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CodeComponent } from "../../../../code/code.component";
+import { UtilService } from '../../../../util.service';
 
 @Component({
   selector: 'app-default-style',
-  imports: [],
+  imports: [CodeComponent],
   templateUrl: './default-style.component.html',
   styleUrl: './default-style.component.css'
 })
-export class DefaultStyleComponent {
+export class DefaultStyleComponent implements OnInit {
+    innerHTML = '';
+    cssCode = '';
 
+    constructor(public util: UtilService){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/one-side-slider-default-style.css').subscribe({
+            next: (response)=>{
+                this.cssCode = response;
+                this.innerHTML = this.util.highlightCssColors(response);
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
