08bab23eae5d3c034d093b6316ae8a40202ea487
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 19:42:49 +0200

    use code component

--- src/app/docs/components/sliders/customize-shape/customize-shape.component.ts
+++ src/app/docs/components/sliders/customize-shape/customize-shape.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../../../../util.service';
+import { CodeComponent } from "../../../../code/code.component";
 
 @Component({
-  selector: 'app-customize-shape',
-  imports: [],
-  templateUrl: './customize-shape.component.html',
-  styleUrl: './customize-shape.component.css'
+    selector: 'app-customize-shape',
+    imports: [CodeComponent],
+    templateUrl: './customize-shape.component.html',
+    styleUrl: './customize-shape.component.css'
 })
-export class CustomizeShapeComponent {
+export class CustomizeShapeComponent implements OnInit{
+innerHTML = '';
+    cssCode = '';
 
+    constructor(public util: UtilService){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/one-side-slider-custom-shape.css').subscribe({
+            next: (response)=>{
+                this.cssCode = response;
+                this.innerHTML = this.util.highlightCssColors(response);
+                console.log(this.cssCode)
+                console.log(this.innerHTML)
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
