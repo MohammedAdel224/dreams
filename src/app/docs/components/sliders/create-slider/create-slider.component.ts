60abd604ff47b32a9dfa37c5b3b3185a350bff43
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 15:10:32 +0200

    read one side slider asset

--- src/app/docs/components/sliders/create-slider/create-slider.component.ts
+++ src/app/docs/components/sliders/create-slider/create-slider.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../../../../util.service';
 
 @Component({
-  selector: 'app-create-slider',
-  imports: [],
-  templateUrl: './create-slider.component.html',
-  styleUrl: './create-slider.component.css'
+    selector: 'app-create-slider',
+    imports: [],
+    templateUrl: './create-slider.component.html',
+    styleUrl: './create-slider.component.css'
 })
-export class CreateSliderComponent {
+export class CreateSliderComponent implements OnInit {
+    oneSideSlider = '';
 
+    constructor(public util: UtilService){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/one-side-slider.html').subscribe({
+            next: (response)=>{
+                this.oneSideSlider = response;
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
