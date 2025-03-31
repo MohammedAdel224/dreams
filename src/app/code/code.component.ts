c88337bb099cbb410b3db965474b65f0552ff041
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 14:38:11 +0200

    implement copy function

--- src/app/code/code.component.ts
+++ src/app/code/code.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { UtilService } from '../util.service';
 
 @Component({
     selector: 'app-code',
@@ -10,4 +11,28 @@ export class CodeComponent {
     @Input() code = '';
     @Input() withContainer = true;
     @Input() class = '';
+    @ViewChild('icon') icon!: ElementRef<HTMLSpanElement>
+
+    timeoutId: NodeJS.Timeout | null = null;
+
+    constructor(public util: UtilService){}
+    copy(){
+        if(this.timeoutId) clearTimeout(this.timeoutId);
+        this.util.copyText(this.code);
+        this.checkCopyIcon();
+        this.timeoutId = setTimeout(()=>{
+            this.uncheckCopyIcon();
+            this.timeoutId = null;
+        }, 5000);
+    }
+
+    checkCopyIcon(){
+        this.icon.nativeElement.classList.replace('bi-clipboard', 'bi-clipboard-check');
+        console.log(this.icon.nativeElement.classList)
+    }
+
+    uncheckCopyIcon(){
+        this.icon.nativeElement.classList.replace('bi-clipboard-check', 'bi-clipboard');
+        console.log(this.icon.nativeElement.classList)
+    }
 }
