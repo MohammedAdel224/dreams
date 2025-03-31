c06400b298d3074e5bb5e08a1d2941b5af8ef2a5
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 18:14:11 +0200

    use code component

--- src/app/docs/components/sliders/customize-colors/customize-colors.component.ts
+++ src/app/docs/components/sliders/customize-colors/customize-colors.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../../../../util.service';
+import { CodeComponent } from "../../../../code/code.component";
 
 @Component({
-  selector: 'app-customize-colors',
-  imports: [],
-  templateUrl: './customize-colors.component.html',
-  styleUrl: './customize-colors.component.css'
+    selector: 'app-customize-colors',
+    imports: [CodeComponent],
+    templateUrl: './customize-colors.component.html',
+    styleUrl: './customize-colors.component.css'
 })
-export class CustomizeColorsComponent {
+export class CustomizeColorsComponent implements OnInit{
+    innerHTML = '';
+    cssCode = '';
 
+    constructor(public util: UtilService){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/one-side-slider-custom-color.css').subscribe({
+            next: (response)=>{
+                this.cssCode = response;
+                this.innerHTML = this.util.highlightCssColors(response);
+                console.log(this.cssCode)
+                console.log(this.innerHTML)
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
