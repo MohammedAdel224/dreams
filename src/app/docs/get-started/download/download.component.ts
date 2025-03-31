9184d465dd91b657df36224d4486128ff452d1ce
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-31 14:49:04 +0200

    read dreams dist content asset

--- src/app/docs/get-started/download/download.component.ts
+++ src/app/docs/get-started/download/download.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrandNameComponent } from '../../../brand-name/brand-name.component';
+import { UtilService } from '../../../util.service';
 
 @Component({
-  selector: 'app-download',
-  imports: [ BrandNameComponent ],
-  templateUrl: './download.component.html',
-  styleUrl: './download.component.css'
+    selector: 'app-download',
+    imports: [ BrandNameComponent ],
+    templateUrl: './download.component.html',
+    styleUrl: './download.component.css'
 })
-export class DownloadComponent {
+export class DownloadComponent implements OnInit {
+    dreamsDistContent = '';
 
+    constructor(public util: UtilService){}
+
+    ngOnInit(): void {
+        this.util.readFile('assets/dreams-dist-content.txt').subscribe({
+            next: (response)=>{
+                this.dreamsDistContent = response;
+            },
+            error: (error)=>{
+                console.log('Error reading file: ', error);
+            }
+        });
+    }
 }
