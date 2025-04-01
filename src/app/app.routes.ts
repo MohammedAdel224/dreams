8d410752cae7e025fe12da236ed0139210186894
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-01 16:40:46 +0200

    rename sliders to one side slider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocsComponent } from './docs/docs.component';
-import { SlidersComponent } from './docs/components/sliders/sliders.component';
+import { OneSideSlider } from './docs/components/one-side-slider/one-side-slider.component';
 import { DownloadComponent } from './docs/get-started/download/download.component';
 import { IntroductionComponent } from './docs/get-started/introduction/introduction.component';
 
@@ -11,7 +11,7 @@ export const routes: Routes = [
         component: DocsComponent,
         children:[
             { path: 'get-started/introduction', component: IntroductionComponent },
-            { path: 'components/one-side-slider', component: SlidersComponent },
+            { path: 'components/one-side-slider', component: OneSideSlider },
             { path: 'get-started/download', component: DownloadComponent},
             { path: '', redirectTo: 'get-started/introduction', pathMatch: 'full'}
         ]
