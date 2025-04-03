2d8cdda98831c3dba093286e8b1e1fe925e6a74e
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-03 20:55:01 +0200

    add navigate to docs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UtilService } from '../util.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    imports: [ RouterModule ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
     constructor(public util: UtilService){}
