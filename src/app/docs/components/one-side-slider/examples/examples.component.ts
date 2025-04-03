3cc34acea29c3cba63eb5a1f9d630f5f90c8dbda
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-03 22:27:01 +0200

    add page number

--- src/app/docs/components/one-side-slider/examples/examples.component.ts
+++ src/app/docs/components/one-side-slider/examples/examples.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Example } from './example.interface';
 import { Example1Component } from './example1/example1.component';
 import { Example2Component } from "./example2/example2.component";
@@ -10,14 +10,21 @@ import { CodeComponent } from "../../../../code/code.component";
     templateUrl: './examples.component.html',
     styleUrl: './examples.component.css'
 })
-export class ExamplesComponent implements AfterViewInit {
+export class ExamplesComponent implements AfterViewInit, OnInit {
     @ViewChild('showContainer', { read: ViewContainerRef }) showContainer!: ViewContainerRef;
     selectedExample!: Example;
+    numberOfPages: number = 0;
+    currentPageNumber = 0;
 
     examples = [
         {component: Example1Component},
         {component: Example2Component}
     ]
+
+    ngOnInit(): void{
+        this.numberOfPages = document.querySelectorAll('.carousel-item').length;
+        this.currentPageNumber = 1;
+    }
 
     ngAfterViewInit(): void {
         this.showExample(this.examples[0]);
@@ -30,5 +37,13 @@ export class ExamplesComponent implements AfterViewInit {
         setTimeout(() => {
             this.selectedExample = componentRef.instance;
         });
+    }
+
+    nextPageNumber(){
+        this.currentPageNumber = this.currentPageNumber % this.numberOfPages + 1;
+    }
+
+    prevoiusPageNumber(){
+        this.currentPageNumber = (this.currentPageNumber - this.numberOfPages - 2) % this.numberOfPages + 1;
     }
 }
