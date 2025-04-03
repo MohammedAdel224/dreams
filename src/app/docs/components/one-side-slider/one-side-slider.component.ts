8cd0f4b22a16fd0f8c547ccd0af1d29be14d2a9f
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-03 16:47:40 +0200

    replace one side slider custom color and shape with examples

--- src/app/docs/components/one-side-slider/one-side-slider.component.ts
+++ src/app/docs/components/one-side-slider/one-side-slider.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { CreateSliderComponent } from './create-slider/create-slider.component';
-import { CustomizeColorsComponent } from './customize-colors/customize-colors.component';
-import { CustomizeShapeComponent } from './customize-shape/customize-shape.component';
 import { DefaultStyleComponent } from './default-style/default-style.component';
 import { BrandNameComponent } from "../../../brand-name/brand-name.component";
+import { ExamplesComponent } from "./examples/examples.component";
 
 @Component({
-  selector: 'app-one-side-slider',
-  imports: [
-    CreateSliderComponent,
-    CustomizeColorsComponent,
-    CustomizeShapeComponent,
-    DefaultStyleComponent,
-    BrandNameComponent
-],
-  templateUrl: './one-side-slider.component.html',
-  styleUrl: './one-side-slider.component.css'
-})
+    selector: 'app-one-side-slider',
+    imports: [
+        CreateSliderComponent,
+        DefaultStyleComponent,
+        BrandNameComponent,
+        ExamplesComponent
+    ],
+    templateUrl: './one-side-slider.component.html',
+    styleUrl: './one-side-slider.component.css'
+    })
 export class OneSideSlider {
 
 }
