bcd7a9609f4e2fbb70f9ad4c7f90061bf9f681d0
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-08 19:31:38 +0200

    add tooltip and change update slider method

--- src/dreams/src/sliders/src/ts/oneSideSlider.ts
+++ src/dreams/src/sliders/src/ts/oneSideSlider.ts
@@ -1,35 +1,58 @@
 import OneSideTrack from "./oneSideTrack";
 import Slider from "./slider";
-import SliderFactory from "./sliderFactory";
+import Tooltip from "./tooltip";
 
 class OneSideSlider{
     private slider: Slider;
     private track: OneSideTrack;
+    private tooltip: Tooltip | null;
 
     constructor(element: HTMLDivElement){
         const track = element.querySelector(".track") as HTMLDivElement;
         const slider = element.querySelector("input.slider[type=range]") as HTMLInputElement;
-        const tooltip = element.querySelector(".tooltip") as HTMLLIElement | null;
+        const tooltip = element.querySelector(".slider-tooltip") as HTMLLIElement | null;
 
         this.track = new OneSideTrack(track);
-        this.slider = SliderFactory.createSlider(slider, tooltip);
+        this.slider = new Slider(slider);
+        this.tooltip = tooltip? new Tooltip(tooltip) : null;
 
-        this.init(track, slider);
+        this.init();
     }
 
-    private init(track: HTMLDivElement, slider: HTMLInputElement){
-        this.slider.addEventListener("input", ()=>{this.fillTrack();});
+    private init(){
+        this.slider.addEventListener("input", ()=>{this.updateSlider();});
+        window.addEventListener("resize", ()=>{this.updateTooltip();});
+        this.updateSlider();
+    }
+
+    private updateSlider(){
         this.fillTrack();
+        this.updateTooltip();
     }
 
     private fillTrack(){
         const trackLength = this.track.width;
         if(trackLength === 0) throw new Error('track lingth is 0');
-        const thumbTrackLength = this.track.width - this.slider.thumbWidth;
+        const thumbTrackLength = trackLength - this.slider.thumbWidth;
         const percent = (this.slider.value - this.slider.min) / (this.slider.max - this.slider.min);
         const offset = (trackLength - thumbTrackLength) / (2 * trackLength);
         const adjustedPercent = thumbTrackLength / trackLength * percent + offset;
+
         this.track.fill(adjustedPercent * 100);
+    }
+
+    private updateTooltip(){
+        if(this.tooltip){
+            this.tooltip.setValue(this.slider.value);
+
+            const trackLength = this.track.width;
+            const thumbWidth = this.slider.thumbWidth;
+            const tooltipTrackLength = trackLength - thumbWidth;
+            const percent = (this.slider.value - this.slider.min) / (this.slider.max - this.slider.min);
+            const newLeft = thumbWidth/2 + percent*tooltipTrackLength;
+
+            this.tooltip.setLeft(newLeft);
+        }
     }
 }
 
