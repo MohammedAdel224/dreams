460afc2c7b8b9c8526c046811b51bb0d050ad722
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-08 22:16:47 +0200

    add create slider with tooltip

--- src/app/docs/components/one-side-slider/one-side-slider.component.ts
+++ src/app/docs/components/one-side-slider/one-side-slider.component.ts
@@ -3,14 +3,16 @@ import { CreateSliderComponent } from './create-slider/create-slider.component';
 import { DefaultStyleComponent } from './default-style/default-style.component';
 import { BrandNameComponent } from "../../../brand-name/brand-name.component";
 import { ExamplesComponent } from "./examples/examples.component";
+import { CreateSliderWithTooltipComponent } from './create-slider-with-tooltip/create-slider-with-tooltip.component';
 
 @Component({
     selector: 'app-one-side-slider',
     imports: [
-        CreateSliderComponent,
-        DefaultStyleComponent,
         BrandNameComponent,
-        ExamplesComponent
+        CreateSliderComponent,
+        ExamplesComponent,
+        CreateSliderWithTooltipComponent,
+        DefaultStyleComponent
     ],
     templateUrl: './one-side-slider.component.html',
     styleUrl: './one-side-slider.component.css'
