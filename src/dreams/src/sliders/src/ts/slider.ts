43f2fd771154d3f172e78327dd6272a049526aaf
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-08 14:07:31 +0200

    change access modifiers

--- src/dreams/src/sliders/src/ts/slider.ts
+++ src/dreams/src/sliders/src/ts/slider.ts
@@ -1,46 +1,46 @@
 import Thumb from "./thumb";
 
 class Slider{
-    #slider: HTMLInputElement;
-    #thumb;
+    protected slider: HTMLInputElement;
+    private thumb;
 
     constructor(element: HTMLInputElement){
-        this.#slider = element;
-        this.#thumb = new Thumb(this);
+        this.slider = element;
+        this.thumb = new Thumb(this);
     }
 
     toElement(): HTMLInputElement{
-        return this.#slider;
+        return this.slider;
     }
 
     addEventListener(type: any, listener: (this: HTMLInputElement, ev: any) => any){
-        this.#slider.addEventListener(type, listener);
+        this.slider.addEventListener(type, listener);
     }
 
     setValue(value: string){
-        this.#slider.value = value;
+        this.slider.value = value;
     }
 
     get value(): number{
-        return Number(this.#slider.value);
+        return Number(this.slider.value);
     }
 
     get min(): number{
-        return Number(this.#slider.min) | 0;
+        return Number(this.slider.min) | 0;
     }
 
     get max(): number{
-        return  Number(this.#slider.max) | 100;
+        return  Number(this.slider.max) | 100;
     }
 
     get width(): number{
-        const style = getComputedStyle(this.#slider);
+        const style = getComputedStyle(this.slider);
         const width = style.getPropertyValue('width');
-        return width ? parseFloat(width) : this.#slider.clientWidth;
+        return width ? parseFloat(width) : this.slider.clientWidth;
     }
 
     get thumbWidth(): number{
-        return this.#thumb.width;
+        return this.thumb.width;
     }
 }
 
