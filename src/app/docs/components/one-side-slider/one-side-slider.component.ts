12746a27be302551522ce345689d764cb4ef4553
Mohammed Adel <mohammedadellma224@gmail.com>
2025-04-10 10:34:44 +0200

    use examples viewer insted of examples component

--- src/app/docs/components/one-side-slider/one-side-slider.component.ts
+++ src/app/docs/components/one-side-slider/one-side-slider.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import { CreateSliderComponent } from './create-slider/create-slider.component';
 import { DefaultStyleComponent } from './default-style/default-style.component';
 import { BrandNameComponent } from "../../../brand-name/brand-name.component";
-import { ExamplesComponent } from "./examples/examples.component";
 import { CreateSliderWithTooltipComponent } from './create-slider-with-tooltip/create-slider-with-tooltip.component';
+import { ExamplesViewerComponent } from "../../../examples-viewer/examples-viewer.component";
+import { Example } from '../../../interfaces/example.interface';
+import { Example1Component } from './examples/example1/example1.component';
+import { Example2Component } from './examples/example2/example2.component';
 
 @Component({
     selector: 'app-one-side-slider',
     imports: [
-        BrandNameComponent,
-        CreateSliderComponent,
-        ExamplesComponent,
-        CreateSliderWithTooltipComponent,
-        DefaultStyleComponent
-    ],
+    BrandNameComponent,
+    CreateSliderComponent,
+    ExamplesViewerComponent,
+    CreateSliderWithTooltipComponent,
+    DefaultStyleComponent,
+],
     templateUrl: './one-side-slider.component.html',
     styleUrl: './one-side-slider.component.css'
     })
-export class OneSideSlider {
+export class OneSideSlider implements OnInit{
+    sliderExamples: Type<Example>[] = [];
 
+    ngOnInit(): void {
+        this.sliderExamples = [Example1Component, Example2Component];
+    }
 }
